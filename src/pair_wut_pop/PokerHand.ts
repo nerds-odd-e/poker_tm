01b26c7043703334793ab4e04f8c94cf0439fb6e
Vorrawut Judasri <vorrawutjud@gmail.com>
2022-11-22 16:24:20 +0700

    TDD-DAY1 wut/pop winrate calculator add second test

--- src/pair_wut_pop/PokerHand.ts
+++ src/pair_wut_pop/PokerHand.ts
@@ -1,5 +1,10 @@
 const getWinrate = (input: String) => {
-    return "Mike:100,Jane:0";
+  const s = input.split(' ');
+  const slice = s.slice(0, 6);
+  if (slice.toString().includes('A')) {
+    return `${s[0]}100,${s[6]}0`;
+  }
+  return `${s[6]}100,${s[0]}0`;
 };
 
 export default getWinrate;
