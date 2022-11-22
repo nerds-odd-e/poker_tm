8c5bf80a2361a2cf8f6df91a42a6f7f52edae6a7
Lee.k <nut@odds.team>
2022-11-22 16:14:33 +0700

    remove only

--- src/poker_air_nutlee/__tests__/record_spec.test.ts
+++ src/poker_air_nutlee/__tests__/record_spec.test.ts
@@ -1,6 +1,6 @@
 import { winRateDetector } from "../services/main";
 
-describe.only("Win-rate Detector", () => {
+describe("Win-rate Detector", () => {
   it("should display winrate of all players", () => {
     // Given
     const gameRecord = "Jane: 8C QS KC 9H 4S Mike: 7D 2S 5D 3S AC";
