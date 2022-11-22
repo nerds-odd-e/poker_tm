324608aa106c4ced0e75bcbd0e85a82034f5374f
Lee.k <nut@odds.team>
2022-11-22 17:15:08 +0700

    extract finding winner logic

--- src/poker_air_nutlee/services/main.ts
+++ src/poker_air_nutlee/services/main.ts
@@ -1,8 +1,21 @@
 const winRateDetector = (record: string) => {
+    
+    const result = findWinner(record)
+
     return [
-        { name: "Jane", rate: 0 },
-        { name: "Mike", rate: 100 },
-    ];
+        { name: result[0].name, rate: 0 },
+        { name: result[1].name, rate: 100 },
+    ]
+}
+
+const findWinner = (record: string) => {
+    const playerSplitted = record.split(' ')
+    const player1Name = playerSplitted[0].replace(':','')
+    const player2Name = playerSplitted[6].replace(':','')
+    return [
+        { name: player1Name, winner: false },
+        { name: player2Name, winner: true },
+    ]
 }
 
 export { winRateDetector };
