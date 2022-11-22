e6d64d4ae4dd18f91fdf80c5a7872ea50e901720
tao-isaman <towlovelove@gmail.com>
2022-11-22 16:49:57 +0700

    update unittest for counting number of played game

--- __tests__/tao_maxim_poker/name_service_spec.ts
+++ __tests__/tao_maxim_poker/name_service_spec.ts
@@ -1,29 +1,40 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import describeWithDB from '../../test_helpers/describeWithDB';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import describeWithDB from "../../test_helpers/describeWithDB";
 
-import { getNames } from '../../src/services/tao_maxim_poker/NameService';
+import { getNames } from "../../src/services/tao_maxim_poker/NameService";
+import { getNamesAndGamesCount } from "../../src/services/tao_maxim_poker/NameService";
 
-describeWithDB('product ', () => {
-
-  it('should return empty list of name for not existing file', async () => {
-    const s = getNames('non-existing-fileName');
+describeWithDB("product ", () => {
+  it("should return empty list of name for not existing file", async () => {
+    const s = getNames("non-existing-fileName");
     expect(s).toStrictEqual(new Set());
-  })
+  });
 
-  it('should return empty list for empty file', async () => {
-    const s = getNames('tao_maxim_file/emptyNameList.txt');
+  it("should return empty list for empty file", async () => {
+    const s = getNames("tao_maxim_file/emptyNameList.txt");
     expect(s).toStrictEqual(new Set());
-  })
+  });
 
-  it('should return names list for single line file', async () => {
-    const s = getNames('tao_maxim_file/singleLine.txt');
-    const expected = ["Jane","Mike"];
+  it("should return names list for single line file", async () => {
+    const s = getNames("tao_maxim_file/singleLine.txt");
+    const expected = ["Jane", "Mike"];
     expect(s).toStrictEqual(new Set(expected));
-  })
+  });
 
-  it('should return names list for multiple line file', async () => {
-    const s = getNames('tao_maxim_file/multipleLine.txt');
-    const expected = ["Jane","Mike","Wu","Ken"];
+  it("should return names list for multiple line file", async () => {
+    const s = getNames("tao_maxim_file/multipleLine.txt");
+    const expected = ["Jane", "Mike", "Wu", "Ken"];
     expect(s).toStrictEqual(new Set(expected));
-  })
+  });
+
+  it("should return names with games played list for multiple line file", async () => {
+    const s = getNamesAndGamesCount("tao_maxim_file/multipleLine.txt");
+    const expected = new Map<string, number>([
+      ["Jane", 999],
+      ["Mike", 998],
+      ["Wu", 2],
+      ["Ken", 1],
+    ]);
+    expect(s).toStrictEqual(expected);
+  });
 });
--- src/services/tao_maxim_poker/NameService.ts
+++ src/services/tao_maxim_poker/NameService.ts
@@ -1,17 +1,32 @@
 import * as fs from "fs";
 
 export function getNames(fileName: string): Set<string> {
-  if (fs.existsSync(fileName)) {
-    const lines = fs.readFileSync(fileName, "utf8");
-
-    return new Set(
-      lines
-        .split(/\r?\n/)
-        .map((line) => extractNames(line))
-        .flatMap((array) => array)
-    );
+  if (!fs.existsSync(fileName)) {
+    return new Set();
   }
-  return new Set();
+
+  return new Set(
+    splitIntoNames(fs.readFileSync(fileName, "utf8")).flatMap((array) => array)
+  );
+}
+
+export function getNamesAndGamesCount(fileName: string): Map<string, number> {
+  if (!fs.existsSync(fileName)) {
+    return new Map();
+  }
+
+  const mapNameAndGame = new Map();
+
+  splitIntoNames(fs.readFileSync(fileName, "utf8")).forEach((lineNames) => {
+    lineNames.forEach((name) => {
+      if (mapNameAndGame.has(name)) {
+        mapNameAndGame.set(name, mapNameAndGame.get(name) + 1);
+      } else {
+        mapNameAndGame.set(name, 1);
+      }
+    });
+  });
+  return mapNameAndGame;
 }
 
 function extractNames(line: string): string[] {
@@ -19,4 +34,8 @@ function extractNames(line: string): string[] {
     .split(/(\s)/)
     .filter((item) => item.endsWith(":"))
     .map((name) => name.substring(0, name.length - 1));
+}
+
+function splitIntoNames(content: string): string[][] {
+  return content.split(/\r?\n/).map((line) => extractNames(line));
 }
