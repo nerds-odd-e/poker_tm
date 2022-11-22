ac099fa0dbcb564fbbe0641a1f86ea1e20a7754f
Newiihuhu <Ninewii_huhu@hotmail.com>
2022-11-22 16:05:21 +0700

    Add test and function for list of player for 1 game

--- __tests__/poker_hands_newii_kan/poker_hands_newii_kan_spec.ts
+++ __tests__/poker_hands_newii_kan/poker_hands_newii_kan_spec.ts
@@ -1,10 +1,21 @@
 import { getListOfPlayers } from '../../src/poker_hands_newii_kan/poker_hands_newii_kan'
 
-describe("List of players", () => {
-    it("should return empty list when get empty list", ()=> {
+describe("CheaterDetector", () => {
+    it("return empty list when no player", () => {
         const games = [];
         const players = getListOfPlayers(games);
         expect(players).toEqual([]);
+    });
+
+    it("return 2 players in list when get 1 game", () => {
+        const games = [
+            {
+                p1: "newii",
+                p2: "kan"
+            }
+        ];
+        const players = getListOfPlayers(games);
+        expect(players).toEqual(["newii", "kan"]);
     });
 
 })
--- src/poker_hands_newii_kan/poker_hands_newii_kan.ts
+++ src/poker_hands_newii_kan/poker_hands_newii_kan.ts
@@ -1,3 +1,16 @@
-export const getListOfPlayers = (games: Array<Object>)=> {
-    return [];
+export const getListOfPlayers = (games: Array<Object>) => {
+    if (games.length == 0) {
+        return [];
+    }
+    // const players = games.map(({game}) => {
+    //     return game.p1;
+    // });
+
+    const players = [];
+    for (let i = 0; i < games.length; i++) {
+        players.push(games[i].p1);
+        players.push(games[i].p2);
+    }
+
+    return players;
 }
