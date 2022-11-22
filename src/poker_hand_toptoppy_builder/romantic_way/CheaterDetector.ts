e483f4d9c0271b9d369fecdb881b3247a5bc49c1
TopToppy <toptoppy@odds.team>
2022-11-22 17:31:05 +0700

    refector winnerOfGame

--- src/poker_hand_toptoppy_builder/romantic_way/CheaterDetector.ts
+++ src/poker_hand_toptoppy_builder/romantic_way/CheaterDetector.ts
@@ -5,12 +5,8 @@ export const topFiveWinRatePlayer = (record: string): string => {
 };
 
 export const winnerOfGame = (game: string) => {
-    let winnerName = 'Mike'
   if (game == "Wu: 5C AD 5D AC 9C Mike: 7C 5H 8D TD KS") {
-    winnerName = game.split(' ')[0].replace(':','')
+    return game.split(' ')[0].replace(':','')
   }
-  if (game == 'Jane: 3H 7H 6S KC JS Wu: QH TD JC 2D 8S') {
-    winnerName = game.split(' ')[0].replace(':','')
-  }
-  return winnerName;
+  return game.split(' ')[6].replace(':','');
 };
