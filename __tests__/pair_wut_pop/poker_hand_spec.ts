c17005f8f09c5ba08de09a6e9b85cb72bc289fc8
Vorrawut Judasri <vorrawutjud@gmail.com>
2022-11-22 17:41:53 +0700

    TDD-DAY1 wut/pop refactor test case

--- __tests__/pair_wut_pop/poker_hand_spec.ts
+++ __tests__/pair_wut_pop/poker_hand_spec.ts
@@ -23,21 +23,23 @@ describe('winrate calculator', () => {
     );
   });
 
-  it('should return Wu (2A higher) with 100% and Mike (2A) 0%', () => {
-    expect(getWinrate('Wu: 5C AD 6D AC 9C Mike: 5D AH 5S AH 8S')).toBe(
-      'Wu:100,Mike:0'
-    );
-  });
+  describe('Both Players got Equal A cards', () => {
+    it('should return Wu (higher 9C) with 100% and Mike 0%', () => {
+      expect(getWinrate('Wu: 5C AD 6D AC 9C Mike: 5D AH 5S AH 8S')).toBe(
+        'Wu:100,Mike:0'
+      );
+    });
 
-  it('should return Wu (2A higher 9) with 100% and Mike (2A) 0%', () => {
-    expect(getWinrate('Mike: 5D AH 5S AH 8S Wu: 5C AD 6D AC 9C')).toBe(
-      'Wu:100,Mike:0'
-    );
-  });
+    it('should return Wu (higher 9C) with 100% and Mike 0% but diffrent position', () => {
+      expect(getWinrate('Mike: 5D AH 5S AH 8S Wu: 5C AD 6D AC 9C')).toBe(
+        'Wu:100,Mike:0'
+      );
+    });
 
-  it('should return Wu (2A higher 6D) with 100% and Mike (2A) 0%', () => {
-    expect(getWinrate('Wu: 5C AD 6D AC 9C Mike: 5D AH 5S AH 9S')).toBe(
-      'Wu:100,Mike:0'
-    );
+    it('should return Wu (higher 6D) with 100% and Mike 0%', () => {
+      expect(getWinrate('Wu: 5C AD 6D AC 9C Mike: 5D AH 5S AH 9S')).toBe(
+        'Wu:100,Mike:0'
+      );
+    });
   });
 });
