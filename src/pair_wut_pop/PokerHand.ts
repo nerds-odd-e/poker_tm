e862e6fa2efd6c8df665a29dbdf128ac501461da
Vorrawut Judasri <vorrawutjud@gmail.com>
2022-11-22 16:54:11 +0700

    TDD-DAY1 wut/pop add test equal double A card

--- src/pair_wut_pop/PokerHand.ts
+++ src/pair_wut_pop/PokerHand.ts
@@ -1,17 +1,21 @@
-function isPlayer1Win(player1_hand: string[], player2_hand: string[]) {
-    return player1_hand.filter(e => e.includes('A')).length > player2_hand.filter(e => e.includes('A')).length;
-}
+const isFirstPlayerWin = (player1_hand: string[], player2_hand: string[]) => {
+  if (
+    player1_hand.filter((e) => e.includes('A')).length >=
+    player2_hand.filter((e) => e.includes('A')).length
+  ) {
+    return true;
+  }
+  return false;
+};
 
 const getWinrate = (input: String) => {
   const s = input.split(' ');
   const player1_hand = s.slice(1, 6);
   const player2_hand = s.slice(7);
-  if (isPlayer1Win(player1_hand, player2_hand)) {
+  if (isFirstPlayerWin(player1_hand, player2_hand)) {
     return `${s[0]}100,${s[6]}0`;
   }
   return `${s[6]}100,${s[0]}0`;
 };
 
 export default getWinrate;
-
-
