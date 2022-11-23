0c3759c2f310cd4cf2ea4ffc75100504f02744f9
tao-isaman <towlovelove@gmail.com>
2022-11-23 10:51:28 +0700

    tcring

---
+++ __tests__/Tao_Premie_may/poker_hand_spec.ts
@@ -0,0 +1,69 @@
+import getWinrate from '../../src/Tao_Premie_may/PokerHand';
+
+const highCardWithHighest = (rank: string) => `2H 3D 4D 5D ${rank}D`;
+
+describe('winrate calculator', () => {
+  it('should return empty when it input empty', () => {
+    expect(
+      getWinrate('')
+    ).toBe('');
+  });
+
+  it('should return Mike with 100% and Jane 0%', () => {
+    expect(
+      getWinrate(
+        `Jane: ${highCardWithHighest('K')} Mike: ${highCardWithHighest('A')}`
+      )
+    ).toBe('Mike:100,Jane:0');
+  });
+
+  it('should return Wu with 100% and Mike 0%', () => {
+    expect(getWinrate('Wu: 5C AD 5D AC 9C Mike: 7C 5H 8D TD KS')).toBe(
+      'Wu:100,Mike:0'
+    );
+  });
+
+  it('should return Wu with 100% and Mike 0%', () => {
+    expect(getWinrate('Mike: 7D 2S 5D 3S AC Wu: 5C AD 5D AH 9C')).toBe(
+      'Wu:100,Mike:0'
+    );
+  });
+
+  it('should return Wu with 100% and Mike 0%', () => {
+    expect(getWinrate('Mike: 7D 2S 5D 3S AC Wu: 5C AD 5D AH 9C,Jane: 5C AD 6D AC 9C Ken: 5D AH 5S KH 8S')).toBe(
+      'Wu:100,Mike:0,Jane:100,Ken:0'
+    );
+  });
+
+  it('should return Mike with 100% and Wu 0% and Jane with 100% and Ken 0%', () => {
+    expect(getWinrate('Wu: 7D 2S 5D 3S AC Mike: 5C AD 5D AH 9C,Jane: 5C AD 6D AC 9C Ken: 5D AH 5S KH 8S')).toBe(
+      'Mike:100,Wu:0,Jane:100,Ken:0'
+    );
+  });
+
+  describe('Both Players got Equal A cards', () => {
+    it('should return Wu (higher 9C) with 100% and Mike 0%', () => {
+      expect(getWinrate('Wu: 5C AD 6D AC 9C Mike: 5D AH 3S AS 8S')).toBe(
+        'Wu:100,Mike:0'
+      );
+    });
+
+    it('should return Wu (higher 9C) with 100% and Mike 0% but diffrent position', () => {
+      expect(getWinrate('Mike: 5D AH 5S AH 8S Wu: 5C AD 6D AC 9C')).toBe(
+        'Wu:100,Mike:0'
+      );
+    });
+
+    it('should return Wu (higher 6D) with 100% and Mike 0%', () => {
+      expect(getWinrate('Wu: 5C AD 6D AC 9C Mike: 5D AH 5S AH 9S')).toBe(
+        'Wu:100,Mike:0'
+      );
+    });
+
+    it('should return Wu (Royal flush) with 100% and Mike 0%', () => {
+      expect(getWinrate('Wu: AC KC QC JC TC Mike: 5D AH 5S AS 9S')).toBe(
+        'Mike:100,Wu:0'
+      );
+    });
+  });
+});
---
+++ src/Tao_Premie_may/PokerHand.ts
@@ -0,0 +1,41 @@
+const totalACard = (cards: string[]) =>
+  cards.filter((e) => e.includes('A')).length;
+
+const isFirstPlayerWin = (player1_hand: string[], player2_hand: string[]) => {
+  if (totalACard(player1_hand) > totalACard(player2_hand)) {
+    return true;
+  }
+  if (totalACard(player1_hand) == totalACard(player2_hand)) {
+    const p1 = player1_hand
+      .filter((e) => !e.includes('A'))
+      .map((e) => Number.parseInt(e.charAt(0)));
+    const p2 = player2_hand
+      .filter((e) => !e.includes('A'))
+      .map((e) => Number.parseInt(e.charAt(0)));
+    return p1.reduce((a, b) => a + b) > p2.reduce((a, b) => a + b);
+  }
+  return false;
+};
+
+const getWinrate = (input: String) => {
+  if (input === '') {
+    return '';
+  }
+  const rounds = input.split(',')
+  const winners = rounds.map(round => getWinner(round))
+
+  return winners.join(",");
+};
+
+const  getWinner = (input: String) => {
+  const s = input.split(' ');
+  const player1_hand = s.slice(1, 6);
+  const player2_hand = s.slice(7);
+  if (isFirstPlayerWin(player1_hand, player2_hand)) {
+    return `${s[0]}100,${s[6]}0`;
+  }
+  return `${s[6]}100,${s[0]}0`;
+}
+
+
+export default getWinrate;
