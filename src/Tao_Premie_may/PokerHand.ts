b028fc553023bb3568ec91f44427669b82de290c
tao-isaman <towlovelove@gmail.com>
2022-11-23 16:05:59 +0700

    tcring

--- src/Tao_Premie_may/PokerHand.ts
+++ src/Tao_Premie_may/PokerHand.ts
@@ -1,7 +1,7 @@
 const totalACard = (cards: string[]) =>
   cards.filter((e) => e.includes("A")).length;
 
-const isFirstPlayerWin = (player2_hand: string[], hand1: Hand) => {
+const isFirstPlayerWin = (hand1: Hand , hand2:Hand) => {
   if (checkFlush(hand1.cards)) {
     return true;
   }
@@ -28,7 +28,7 @@ const getWinner = (gameRaw: String) => {
   const s = gameRaw.split(" ");
   const player1_hand = s.slice(1, 6);
   const player2_hand = s.slice(7);
-  if (isFirstPlayerWin(player2_hand, new Hand(player1_hand))) {
+  if (isFirstPlayerWin(new Hand(player1_hand), new Hand(player2_hand))) {
     return `${s[0]}100,${s[6]}0`;
   }
   return `${s[6]}100,${s[0]}0`;
