e773fa805a5f7de75bbfd9192afdaceb7ca421b1
Air Chanita Anuwong <airmee@cave.local>
2022-11-23 16:33:11 +0700

    tcring

--- src/services/tao_maxim_poker/NameService.ts
+++ src/services/tao_maxim_poker/NameService.ts
@@ -1,69 +1,11 @@
 import * as fs from "fs";
 import { Types } from "mongoose";
 
-export function getNames(fileName: string): Set<string> {
-  if (!fs.existsSync(fileName)) {
-    return new Set();
-  }
-
-  return new Set(
-    splitIntoNames(fs.readFileSync(fileName, "utf8")).flatMap((array) => array)
-  );
-}
-
-export function getNamesAndGamesCount(fileName: string): Map<string, number> {
-  if (!fs.existsSync(fileName)) {
-    return new Map();
-  }
-
-  const mapNameAndGame = new Map();
-
-  splitIntoNames(fs.readFileSync(fileName, "utf8")).forEach((lineNames) => {
-    lineNames.forEach((name) => {
-      if (mapNameAndGame.has(name)) {
-        mapNameAndGame.set(name, mapNameAndGame.get(name) + 1);
-      } else {
-        mapNameAndGame.set(name, 1);
-      }
-    });
-  });
-  return mapNameAndGame;
-}
-
 function extractNames(line: string): string[] {
   return line
     .split(/(\s)/)
     .filter((item) => item.endsWith(":"))
     .map((name) => name.substring(0, name.length - 1));
-}
-
-function splitIntoNames(content: string): string[][] {
-  return content.split(/\r?\n/).map((line) => extractNames(line));
-}
-
-export function getNamesAndWinCount(fileName: string): Map<string, number> {
-  if (!fs.existsSync(fileName)) {
-    return new Map();
-  }
-
-  const allResults = new Map<string,number>();
-  const content = fs.readFileSync(fileName, "utf8");
-  content
-    .split(/\r?\n/)
-    .map((game) => play(game))
-    .forEach((result) => {
-        for (let name of result.keys()) {
-            const res = result.get(name) as number;
-            if (allResults.has(name)) {
-                const allRes = allResults.get(name) as number;
-                allResults.set(name, allRes + res);
-            } else {
-                allResults.set(name, res);
-            }
-        }
-    });
-
-  return allResults;
 }
 
 export function getNamesAndWinRate(fileName: string): Map<string, number> {
