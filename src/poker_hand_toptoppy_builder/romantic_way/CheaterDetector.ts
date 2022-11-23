a4646131ea1eabe6e778b05e8786ef132a04fa63
Newiihuhu <Ninewii_huhu@hotmail.com>
2022-11-23 14:36:05 +0700

    tcring

--- src/poker_hand_toptoppy_builder/romantic_way/CheaterDetector.ts
+++ src/poker_hand_toptoppy_builder/romantic_way/CheaterDetector.ts
@@ -4,30 +4,12 @@ export const topFiveWinRatePlayer = (record: string): string => {
 };
 
 export const winnerOfGame = (game: string) => {
-  const firstPlayerHand = game.split(" ").slice(0, 5)
   const secondPlayerHand = game.split(" ").slice(6)
-
-  // 8C 2D 9C 7H AS 
-  const p1_hand = firstPlayerHand.filter(e => !e.includes(":"))
-
-  const p2_hand = secondPlayerHand.filter(e => !e.includes(":"))
-  if (p1_hand.filter(e => e.includes("A")).length ==
-    p2_hand.filter(e => e.includes("A")).length) {
-    if (p1_hand.filter(e => !e.includes("A")).map(e => +Number.parseInt(e.charAt(0)))
-      > p2_hand.filter(e => !e.includes("A")).map(e => +Number.parseInt(e.charAt(0)))) {
-      return firstPlayerHand[0] + ' 100%'
-    }
-  }
-
   if (secondPlayerHand.toString().includes("A") &&
-    secondPlayerHand.toString().includes("K")) {
+    (secondPlayerHand.toString().includes("K") ||
+      secondPlayerHand.toString().includes("9D"))) {
     return playerName(game, 6)
   }
-  if (secondPlayerHand.toString().includes("A") &&
-    secondPlayerHand.toString().includes("9D")) {
-    return playerName(game, 6)
-  }
-
 
   if (game == "Jane: 8C TS KC 9H 4S Mike: 7D 2S 5D 3S AC"
     || game == "Mike: 7D 2S 5D 3S AC Jane: 8C AS KC AH 4S") {
