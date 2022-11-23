0ccc5a7eedbf18845ca106782f2b0bf4ed087676
laksana angwattananon <laksana.ang@laksanas-MacBook-Air.local>
2022-11-23 10:31:41 +0700

    new project jean and p'kan

---
+++ __tests__/tao_maxim_poker_jean_kan/name_service_spec.ts
@@ -0,0 +1,52 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import describeWithDB from "../../test_helpers/describeWithDB";
+
+import { getNames, getNamesAndWinCount } from "../../src/services/tao_maxim_poker/NameService";
+import { getNamesAndGamesCount } from "../../src/services/tao_maxim_poker/NameService";
+
+describeWithDB("product ", () => {
+  it("should return empty list of name for not existing file", async () => {
+    const s = getNames("non-existing-fileName");
+    expect(s).toStrictEqual(new Set());
+  });
+
+  it("should return empty list for empty file", async () => {
+    const s = getNames("tao_maxim_file/emptyNameList.txt");
+    expect(s).toStrictEqual(new Set());
+  });
+
+  it("should return names list for single line file", async () => {
+    const s = getNames("tao_maxim_file/singleLine.txt");
+    const expected = ["Jane", "Mike"];
+    expect(s).toStrictEqual(new Set(expected));
+  });
+
+  it("should return names list for multiple line file", async () => {
+    const s = getNames("tao_maxim_file/multipleLine.txt");
+    const expected = ["Jane", "Mike", "Wu", "Ken"];
+    expect(s).toStrictEqual(new Set(expected));
+  });
+
+  it("should return names with games played list for multiple line file", async () => {
+    const s = getNamesAndGamesCount("tao_maxim_file/multipleLine.txt");
+    const expected = new Map<string, number>([
+      ["Mike", 998],
+      ["Jane", 999],
+      ["Wu", 2],
+      ["Ken", 1],
+    ]);
+    expect(s).toStrictEqual(expected);
+  });
+
+  it("should return empty played game list for empty file", async () => {
+    const s = getNamesAndGamesCount("tao_maxim_file/emptyNameList.txt");
+    const expected = new Map<string, number>();
+    expect(s).toStrictEqual(expected);
+  });
+
+  it("should return amount of game won by player for single line file", async () => {
+    const s = getNamesAndWinCount("tao_maxim_file/singleLine.txt");
+    expect(s.get("Jane")).toBe(1);
+  });  
+});
+
