8b5ad647abc343c70de3b8a2dbc8484389aaa305
TopToppy <toptoppy@odds.team>
2022-11-23 15:17:10 +0700

    tcring

--- src/poker_air_nutlee/__tests__/record_spec.test.ts
+++ src/poker_air_nutlee/__tests__/record_spec.test.ts
@@ -9,53 +9,76 @@ describe("Win-rate Detector", () => {
     const result = play(gameRecord);
 
     // Then
-    expect(result).toContainEqual(
-        { name: "Jane", winRate: 0, gameCount: 1, winCount: 0}
-    );
-    expect(result).toContainEqual(
-        { name: "Mike", winRate: 100, gameCount: 1, winCount: 1}
-    );
+    expect(result).toContainEqual({
+      name: "Jane",
+      winRate: 0,
+      gameCount: 1,
+      winCount: 0,
+    });
+    expect(result).toContainEqual({
+      name: "Mike",
+      winRate: 100,
+      gameCount: 1,
+      winCount: 1,
+    });
   });
 
   it("should display win-rate of all players when player2 wins in multiple games", () => {
     // Given
-    const gameRecords = ["Jane: 3H 7H 6S KC JS Wu: QH TD JC 2D 8S",
-        "Jane: TH 8H 5C QS TC Mike: 9H 4D JC KS JS"]
+    const gameRecords = [
+      "Jane: 3H 7H 6S KC JS Wu: QH TD JC 2D 8S",
+      "Jane: TH 8H 5C QS TC Mike: 9H 4D JC KS JS",
+    ];
 
     // When
     const result = process(gameRecords);
 
     //Then
-    expect(result).toContainEqual(
-        { name: "Jane", winRate: 0, gameCount: 2, winCount: 0}
-    );
-    expect(result).toContainEqual(
-        { name: "Wu", winRate: 100, gameCount: 1, winCount: 1}
-    );
-    expect(result).toContainEqual(
-        { name: "Mike", winRate: 100, gameCount: 1, winCount: 1}
-    );
+    expect(result).toContainEqual({
+      name: "Jane",
+      winRate: 0,
+      gameCount: 2,
+      winCount: 0,
+    });
+    expect(result).toContainEqual({
+      name: "Wu",
+      winRate: 100,
+      gameCount: 1,
+      winCount: 1,
+    });
+    expect(result).toContainEqual({
+      name: "Mike",
+      winRate: 100,
+      gameCount: 1,
+      winCount: 1,
+    });
   });
 
   it("should display win-rate of all players when player2 wins in single game", () => {
     // Given
-    const gameRecords = ["Jane: 3H 7H 6S KC JS Wu: QH TD JC 2D 8S"]
+    const gameRecords = ["Jane: 3H 7H 6S KC JS Wu: QH TD JC 2D 8S"];
 
     // When
     const result = process(gameRecords);
 
     //Then
-    expect(result).toContainEqual(
-        { name: "Jane", winRate: 0, gameCount: 1, winCount: 0}
-    );
-    expect(result).toContainEqual(
-        { name: "Wu", winRate: 100, gameCount: 1, winCount: 1}
-    );
+    expect(result).toContainEqual({
+      name: "Jane",
+      winRate: 0,
+      gameCount: 1,
+      winCount: 0,
+    });
+    expect(result).toContainEqual({
+      name: "Wu",
+      winRate: 100,
+      gameCount: 1,
+      winCount: 1,
+    });
   });
 
   it("should not display win-rate of all players when games list is empty", () => {
     // Given
-    const gameRecords = []
+    const gameRecords = [];
 
     // When
     const result = process(gameRecords);
@@ -66,39 +89,46 @@ describe("Win-rate Detector", () => {
 
   it("should display win-rate of all players when player1 wins game", () => {
     // Given
-    const gameRecords = ["Jane: AH TD JC 2D 8S Wu: 3H 7H 6S KC JS"]
+    const gameRecords = ["Jane: AH TD JC 2D 8S Wu: 3H 7H 6S KC JS"];
 
     // When
     const result = process(gameRecords);
 
     //Then
-    expect(result).toContainEqual(
-        { name: "Wu", winRate: 100, gameCount: 1, winCount: 1}
-    );
-    expect(result).toContainEqual(
-        { name: "Jane", winRate: 0, gameCount: 1, winCount: 0}
-    );
+    expect(result).toContainEqual({
+      name: "Wu",
+      winRate: 100,
+      gameCount: 1,
+      winCount: 1,
+    });
+    expect(result).toContainEqual({
+      name: "Jane",
+      winRate: 0,
+      gameCount: 1,
+      winCount: 0,
+    });
   });
 
   it("should comprae card by value", () => {
     const sortedDeck: Card[] = [
-        new Card("AS"),
-        new Card("KS"),
-        new Card("QS"),
-        new Card("JS"),
-        new Card("TS"),
-        new Card("9S"),
-        new Card("8S"),
-        new Card("7S"),
-        new Card("6S"),
-        new Card("5S"),
-        new Card("4S"),
-        new Card("3S"),
-        new Card("2S")
-    ]    
+      new Card("AS"),
+      new Card("KS"),
+      new Card("QS"),
+      new Card("JS"),
+      new Card("TS"),
+      new Card("9S"),
+      new Card("8S"),
+      new Card("7S"),
+      new Card("6S"),
+      new Card("5S"),
+      new Card("4S"),
+      new Card("3S"),
+      new Card("2S"),
+    ];
 
     //add shuffle - ????
-    const sorted = sortedDeck.sort(compareCards)
+    const shuffledDeck = sortedDeck.sort(() => Math.random() - 0.5);
+    const sorted = shuffledDeck.sort(compareCards);
     expect(sorted).toBe(sortedDeck);
-  })
+  });
 });
