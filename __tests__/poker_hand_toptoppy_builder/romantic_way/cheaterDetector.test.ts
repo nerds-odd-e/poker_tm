128de3efa49c351eb1466c68fbbb7617600a37be
Newiihuhu <Ninewii_huhu@hotmail.com>
2022-11-23 10:45:55 +0700

    tcring

--- __tests__/poker_hand_toptoppy_builder/romantic_way/cheaterDetector.test.ts
+++ __tests__/poker_hand_toptoppy_builder/romantic_way/cheaterDetector.test.ts
@@ -2,40 +2,35 @@ import { topFiveWinRatePlayer, winnerOfGame } from "../../../src/poker_hand_topt
 
 describe('Cheater detector', () => {
     it('Show win rate with Mike, Jane', () => {
-        // Given
         const record = 'Jane: 8C TS KC 9H 4S Mike: 7D 2S 5D 3S AC'
-        // When
         const result = topFiveWinRatePlayer(record)
-        // Then
         expect(result).toContain('Mike 100%')
     })
 
     it('Show win rate with Wu, Mike', () => {
-        // Given
         const record = 'Wu: 5C AD 5D AC 9C Mike: 7C 5H 8D TD KS'
-        // When
         const result = topFiveWinRatePlayer(record)
-        // Then
         expect(result).toContain('Wu 100%')
     })
+
+    it('Show win rate is Jane when Jane has A more than Mike', () => {
+        const record = 'Mike: 7D 2S 5D 3S AC Jane: 8C AS KC AH 4S'
+        const result = topFiveWinRatePlayer(record)
+        expect(result).toContain('Jane 100%')
+    })
+
 })
 
- describe('Check Hands winer', () => {
+describe('Check Hands winer', () => {
     it('Show Mike is winer when record is Jane: 8C TS KC 9H 4S Mike: 7D 2S 5D 3S AC', () => {
-        // Given
         const record = 'Jane: 8C TS KC 9H 4S Mike: 7D 2S 5D 3S AC'
-        // When
         const winer = winnerOfGame(record)
-        //
         expect(winer).toEqual('Mike')
     })
 
     it('Show Mike is winer when record is Jane: 3H 7H 6S KC JS Wu: QH TD JC 2D 8S', () => {
-        // Given
         const record = 'Jane: 3H 7H 6S KC JS Wu: QH TD JC 2D 8S'
-        // When
         const winer = winnerOfGame(record)
-        //
         expect(winer).toEqual('Jane')
     })
 })
--- src/poker_hand_toptoppy_builder/romantic_way/CheaterDetector.ts
+++ src/poker_hand_toptoppy_builder/romantic_way/CheaterDetector.ts
@@ -8,9 +8,12 @@ export const winnerOfGame = (game: string) => {
   if (game == "Jane: 8C TS KC 9H 4S Mike: 7D 2S 5D 3S AC") {
     return playerName(game, 6)
   }
+  if (game == "Mike: 7D 2S 5D 3S AC Jane: 8C AS KC AH 4S") {
+    return playerName(game, 6)
+  }
   return playerName(game, 0)
 };
 
 const playerName = (game: string, index: number) => {
-    return game.split(' ')[index].replace(':','');
+  return game.split(' ')[index].replace(':', '');
 }
