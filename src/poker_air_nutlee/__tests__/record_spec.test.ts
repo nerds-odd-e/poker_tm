d4612771bbf67a9553aaa9b5af9d3ae212fd2e53
TopToppy <toptoppy@odds.team>
2022-11-23 15:18:55 +0700

    tcring

--- src/poker_air_nutlee/__tests__/record_spec.test.ts
+++ src/poker_air_nutlee/__tests__/record_spec.test.ts
@@ -128,7 +128,7 @@ describe("Win-rate Detector", () => {
 
     //add shuffle - ????
     const shuffledDeck = sortedDeck.sort(() => Math.random() - 0.5);
-    shuffledDeck.forEach(e => console.log(`${e}`))
+    shuffledDeck.forEach(e => console.log(`${e.value}`))
     const sorted = shuffledDeck.sort(compareCards);
     expect(sorted).toBe(sortedDeck);
   });
