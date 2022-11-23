3a6773dc1363d96e6bc253cdcc8e9869b9f90fcf
Jirat Chokdeeanan <jirat.cho@gmail.com>
2022-11-23 14:57:45 +0700

    tcring

--- __tests__/poker_miwtoo_jean/poker_3.spec.ts
+++ __tests__/poker_miwtoo_jean/poker_3.spec.ts
@@ -5,28 +5,34 @@ import { winnerDetector2 } from "../../src/poker_miwtoo_jean/poker";
 describe('CheaterDetector', () => {
 
     it("should return player whos has higher high card (8C TS KC 9H 4S) vs (7D 2S 5D 3S AC)", () => {
-        expect(winnerDetector2()).toEqual([
+        const filePath = path.join(__dirname, '../poker_miwtoo_jean/data/data1.txt');
+        const buffer = fs.readFileSync(filePath, "utf8");
+        const file = buffer.toString();
+        expect(winnerDetector2(file)).toEqual([
             {
-                name: "Jane",
+                name: "Mike",
                 winrate: 100
             }, 
             {
-                name: "Mike",
+                name: "Jane",
                 winrate: 0
             },
         ])
     })
 
     it("should return player whos has higher high card (7C 9C 6D KD 3H) vs (4C QS TC AC KH)", () => {
-        expect(winnerDetector2()).toEqual([
+        const filePath = path.join(__dirname, '../poker_miwtoo_jean/data/data2.txt');
+        const buffer = fs.readFileSync(filePath, "utf8");
+        const file = buffer.toString();
+        expect(winnerDetector2(file)).toEqual([
             {
-                name: "Jane",
+                name: "Mike",
                 winrate: 100
             }, 
             {
-                name: "Mike",
+                name: "Jane",
                 winrate: 0
-            },
+            }
         ])
     })
 
--- src/poker_miwtoo_jean/poker.ts
+++ src/poker_miwtoo_jean/poker.ts
@@ -1,6 +1,22 @@
 import * as fs from "fs";
 import * as path from "path";
 
+const cardFace: Map<string, number> = new Map([
+    ['A', 14],
+    ['K', 13],
+    ['Q', 12],
+    ['J', 11],
+    ['T', 10],
+    ['9', 9],
+    ['8', 8],
+    ['7', 7],
+    ['6', 6],
+    ['5', 5],
+    ['4', 4],
+    ['3', 3],
+    ['2', 2]
+]);
+
 function main() {
 
 
@@ -58,11 +74,11 @@ export function winnerDetector(data: string) {
         if (round[i] != '') {
             const splitSpace = round[i].split(" ")
             const player1 = splitSpace[0].replace(':', '');
-            const player1Hands = splitSpace.slice(1,6).toString();
+            const player1Hands = splitSpace.slice(1, 6).toString();
 
             const player2 = splitSpace[6].replace(':', '');
-            const player2Hands = splitSpace.slice(7,12).toString();
-            if(isPair(player1Hands)) {
+            const player2Hands = splitSpace.slice(7, 12).toString();
+            if (isPair(player1Hands)) {
                 return player1
             }
 
@@ -90,15 +106,62 @@ const isThreeOfAKind = (cards: string) => {
     return cards == '7H,7D,7C,3S,AC';
 }
 
-export function winnerDetector2() {
-    return ([
-        {
-            name: "Jane",
-            winrate: 100
-        }, 
-        {
-            name: "Mike",
-            winrate: 0
-        },
-    ])
+export function winnerDetector2(data: String) {
+    const round = data.toString().split("\n");
+    for (const i in round) {
+        if (round[i] != '') {
+            const splitSpace = round[i].split(" ")
+            const player1 = splitSpace[0].replace(':', '');
+            const player1Hands = splitSpace.slice(1, 6);
+
+            let maxPlayer1Point = 0
+            player1Hands.forEach(
+                hand => {
+                    const number = hand.charAt(0)
+                    const point = cardFace.get(number) || 0
+                    if (point > maxPlayer1Point) maxPlayer1Point = point
+                }
+            )
+
+            const player2 = splitSpace[6].replace(':', '');
+            const player2Hands = splitSpace.slice(7, 12);
+
+            let maxPlayer2Point = 0
+            player2Hands.forEach(
+                hand => {
+                    const number = hand.charAt(0)                    
+                    const point = cardFace.get(number) || 0
+                    if (point > maxPlayer2Point) maxPlayer2Point = point
+                }
+            )
+
+            if (maxPlayer1Point > maxPlayer2Point) {
+                return [
+                    {
+                        name: player1,
+                        winrate: 100
+                    },
+                    {
+                        name: player2,
+                        winrate: 0
+                    }
+                ]
+            } 
+            
+            if (maxPlayer2Point > maxPlayer1Point) {
+                return [
+                    {
+                        name: player2,
+                        winrate: 100
+                    },
+                    {
+                        name: player1,
+                        winrate: 0
+                    }
+                ]
+            }
+        }
+    }
+
 }
+
