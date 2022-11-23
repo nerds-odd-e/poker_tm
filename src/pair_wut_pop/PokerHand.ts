15782077612a887d1a23c343c1ad45d041a5d382
Miwtoo <miwyugioh@gmail.com>
2022-11-23 11:05:07 +0700

    tcring

--- src/pair_wut_pop/PokerHand.ts
+++ src/pair_wut_pop/PokerHand.ts
@@ -17,24 +17,27 @@ const isFirstPlayerWin = (player1_hand: string[], player2_hand: string[]) => {
   return false;
 };
 
-const getWinrate = (input: String) => {
-  if (input === '') {
+const getWinrate = (game: String) => {
+  if (game === '') {
     return '';
   }
-  const rounds = input.split(',')
+  const rounds = game.split(',')
   const winners = rounds.map(round => getWinner(round))
 
   return winners.join(",");
 };
 
-const  getWinner = (input: String) => {
-  const s = input.split(' ');
+const  getWinner = (game: String) => {
+  const s = game.split(' ');
   const player1_hand = s.slice(1, 6);
   const player2_hand = s.slice(7);
   if (isFirstPlayerWin(player1_hand, player2_hand)) {
     return `${s[0]}100,${s[6]}0`;
   }
-  return `${s[6]}100,${s[0]}0`;
+  const xxx = {
+    playerNames: [s[6], s[0]]
+  }
+  return `${xxx.playerNames[0]}100,${xxx.playerNames[1]}0`;
 }
 
 
