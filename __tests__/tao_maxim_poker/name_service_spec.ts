fb4373d3cc2c44c456de8fe1dc4cff39ddeb42c3
Air Chanita Anuwong <airmee@cave.local>
2022-11-23 16:38:00 +0700

    tcring

--- __tests__/tao_maxim_poker/name_service_spec.ts
+++ __tests__/tao_maxim_poker/name_service_spec.ts
@@ -3,31 +3,6 @@ import describeWithDB from "../../test_helpers/describeWithDB";
 
 import { getNamesAndWinRate } from "../../src/services/tao_maxim_poker/NameService";
 
-// describeWithDB("file reader ", () => {
-//   it("should return empty list of name for not existing file", async () => {
-//     const s = getNames("non-existing-fileName");
-//     expect(s).toStrictEqual(new Set());
-//   });
-
-//   it("should return empty list for empty file", async () => {
-//     const s = getNames("tao_maxim_file/emptyNameList.txt");
-//     expect(s).toStrictEqual(new Set());
-//   });
-
-//   it("should return names list for single line file", async () => {
-//     const s = getNames("tao_maxim_file/singleLine.txt");
-//     const expected = ["Jane", "Mike"];
-//     expect(s).toStrictEqual(new Set(expected));
-//   });
-
-//   it("should return names list for multiple line file", async () => {
-//     const s = getNames("tao_maxim_file/multipleLine.txt");
-//     const expected = ["Jane", "Mike", "Wu", "Ken"];
-//     expect(s).toStrictEqual(new Set(expected));
-//   });
-// });
-
-
 describeWithDB("WinRateCollector ", () => {
 
   it("should return names list for empty file", () => {
