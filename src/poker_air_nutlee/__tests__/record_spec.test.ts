c66ca6a46e243640501279f75fbd6de1d71f16f6
TopToppy <toptoppy@odds.team>
2022-11-23 10:58:51 +0700

    tcring

--- src/poker_air_nutlee/__tests__/record_spec.test.ts
+++ src/poker_air_nutlee/__tests__/record_spec.test.ts
@@ -1,29 +1,32 @@
 import { winRateDetector } from "../services/main";
 
 describe("Win-rate Detector", () => {
-    it("should display win-rate of all players when player2 wins.", () => {
-        // Given
-        const gameRecord = "Jane: 8C QS KC 9H 4S Mike: 7D 2S 5D 3S AC";
+  it("should display win-rate of all players when player2 wins.", () => {
+    // Given
+    const gameRecord = "Jane: 8C QS KC 9H 4S Mike: 7D 2S 5D 3S AC";
 
-        // Then
-        expect(winRateDetector(gameRecord)).toContainEqual(
-            { name: "Jane", winRate: 0 },
-        );
-        expect(winRateDetector(gameRecord)).toContainEqual(
-            { name: "Mike", winRate: 100 },
-        );
+    // When
+    const result = winRateDetector(gameRecord);
+
+    // Then
+    expect(result[0].name).toBe("Jane");
+    expect(result[0].winRate).toBe(0);
+    expect(result[1].name).toBe("Mike");
+    expect(result[1].winRate).toBe(100);
+  });
+
+  xit("should display winrate of all players when player1 wins.", () => {
+    // Given
+    const gameRecord = "Wu: AC QS KC 9H 4S Ken: 7D 2S 5D 3S KH";
+
+    // Then
+    expect(winRateDetector(gameRecord)).toContainEqual({
+      name: "Wu",
+      winRate: 100,
     });
-
-    xit("should display winrate of all players when player1 wins.", () => {
-        // Given
-        const gameRecord = "Wu: AC QS KC 9H 4S Ken: 7D 2S 5D 3S KH";
-
-        // Then
-        expect(winRateDetector(gameRecord)).toContainEqual(
-            { name: "Wu", winRate: 100 }
-        );
-        expect(winRateDetector(gameRecord)).toContainEqual(
-            { name: "Ken", winRate: 0 }
-        );
+    expect(winRateDetector(gameRecord)).toContainEqual({
+      name: "Ken",
+      winRate: 0,
     });
+  });
 });
--- src/poker_air_nutlee/services/main.ts
+++ src/poker_air_nutlee/services/main.ts
@@ -1,58 +1,66 @@
-
 class PlayerCollection {
-    name: string;
-    winningCount: number;
-    gameCount: number;
-    winRate: number;
+  name: string;
+  winningCount: number;
+  gameCount: number;
+  winRate: number;
 }
 
 class GameResult {
-    name: string;
-    winner: boolean;
+  name: string;
+  winner: boolean;
 }
 
 const winRateDetector = (game: string) => {
-    const playerCollection: PlayerCollection[] = []
-    const gameResult = play(game) as GameResult[]
+  const playerCollection: PlayerCollection[] = [];
+  const gameResult = play(game) as GameResult[];
 
-    const player1Collection = playerCollection.find(player => player.name === gameResult[0].name) as PlayerCollection
-    let player1result = getPlayerResult(player1Collection, gameResult[0])
-    
-    const player2Collection = playerCollection.find(player => player.name === gameResult[1].name) as PlayerCollection
-    let player2result = getPlayerResult(player2Collection, gameResult[1])
-    
-    return [
-        { name: player1result.name, winRate: player1result.winRate },
-        { name: player2result.name, winRate: player2result.winRate },
-    ]
-}
+  const player1Collection = playerCollection.find(
+    (player) => player.name === gameResult[0].name
+  ) as PlayerCollection;
+  let player1result = getPlayerResult(player1Collection, gameResult[0]);
+
+  const player2Collection = playerCollection.find(
+    (player) => player.name === gameResult[1].name
+  ) as PlayerCollection;
+  let player2result = getPlayerResult(player2Collection, gameResult[1]);
+
+  return [
+    { name: player1result.name, winRate: player1result.winRate },
+    { name: player2result.name, winRate: player2result.winRate },
+  ];
+};
 
 const play = (game: string) => {
-    const playerSplitted = game.split(' ')
-    const player1Name = playerSplitted[0].replace(':', '')
-    const player2Name = playerSplitted[6].replace(':', '')
-    return [
-        { name: player1Name, winner: false },
-        { name: player2Name, winner: true },
-    ]
-}
+  const playerSplitted = game.split(" ");
+  const player1Name = playerSplitted[0].replace(":", "");
+  const player2Name = playerSplitted[6].replace(":", "");
+  return [
+    { name: player1Name, winner: false },
+    { name: player2Name, winner: true },
+  ];
+};
 
-const getPlayerResult = (playerCollection: PlayerCollection, findWinnerResult: GameResult ) => {
-    if (playerCollection) {
-        playerCollection.gameCount++
-        playerCollection.winningCount = findWinnerResult.winner ? playerCollection.winningCount + 1 : playerCollection.winningCount
-        playerCollection.winRate = playerCollection.winningCount / playerCollection.gameCount * 100
-        return playerCollection
-    }
-    else {
-        const player2Data = {
-            name: findWinnerResult.name,
-            winningCount: findWinnerResult.winner ? 1 : 0,
-            gameCount: 1,
-            winRate: findWinnerResult.winner ? 100 : 0,
-        }
-        return player2Data
-    }
-}
+const getPlayerResult = (
+  playerCollection: PlayerCollection,
+  findWinnerResult: GameResult
+) => {
+  if (playerCollection) {
+    playerCollection.gameCount++;
+    playerCollection.winningCount = findWinnerResult.winner
+      ? playerCollection.winningCount + 1
+      : playerCollection.winningCount;
+    playerCollection.winRate =
+      (playerCollection.winningCount / playerCollection.gameCount) * 100;
+    return playerCollection;
+  } else {
+    const player2Data = {
+      name: findWinnerResult.name,
+      winningCount: findWinnerResult.winner ? 1 : 0,
+      gameCount: 1,
+      winRate: findWinnerResult.winner ? 100 : 0,
+    };
+    return player2Data;
+  }
+};
 
 export { winRateDetector };
