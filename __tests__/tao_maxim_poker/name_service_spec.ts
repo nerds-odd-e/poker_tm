758d48d1a4f4206ec4ae42d9f8388a236176c876
Air Chanita Anuwong <airmee@cave.local>
2022-11-23 16:31:20 +0700

    tcring

--- __tests__/tao_maxim_poker/name_service_spec.ts
+++ __tests__/tao_maxim_poker/name_service_spec.ts
@@ -1,60 +1,31 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import describeWithDB from "../../test_helpers/describeWithDB";
 
-import { getNames, getNamesAndWinCount } from "../../src/services/tao_maxim_poker/NameService";
-import { getNamesAndGamesCount, getNamesAndWinRate } from "../../src/services/tao_maxim_poker/NameService";
+import { getNamesAndWinRate } from "../../src/services/tao_maxim_poker/NameService";
 
-describeWithDB("product ", () => {
-  it("should return empty list of name for not existing file", async () => {
-    const s = getNames("non-existing-fileName");
-    expect(s).toStrictEqual(new Set());
-  });
+// describeWithDB("file reader ", () => {
+//   it("should return empty list of name for not existing file", async () => {
+//     const s = getNames("non-existing-fileName");
+//     expect(s).toStrictEqual(new Set());
+//   });
 
-  it("should return empty list for empty file", async () => {
-    const s = getNames("tao_maxim_file/emptyNameList.txt");
-    expect(s).toStrictEqual(new Set());
-  });
+//   it("should return empty list for empty file", async () => {
+//     const s = getNames("tao_maxim_file/emptyNameList.txt");
+//     expect(s).toStrictEqual(new Set());
+//   });
 
-  it("should return names list for single line file", async () => {
-    const s = getNames("tao_maxim_file/singleLine.txt");
-    const expected = ["Jane", "Mike"];
-    expect(s).toStrictEqual(new Set(expected));
-  });
+//   it("should return names list for single line file", async () => {
+//     const s = getNames("tao_maxim_file/singleLine.txt");
+//     const expected = ["Jane", "Mike"];
+//     expect(s).toStrictEqual(new Set(expected));
+//   });
 
-  it("should return names list for multiple line file", async () => {
-    const s = getNames("tao_maxim_file/multipleLine.txt");
-    const expected = ["Jane", "Mike", "Wu", "Ken"];
-    expect(s).toStrictEqual(new Set(expected));
-  });
-
-  it("should return names with games played list for multiple line file", async () => {
-    const s = getNamesAndGamesCount("tao_maxim_file/multipleLine.txt");
-    const expected = new Map<string, number>([
-      ["Mike", 998],
-      ["Jane", 999],
-      ["Wu", 2],
-      ["Ken", 1],
-    ]);
-    expect(s).toStrictEqual(expected);
-  });
-
-  it("should return empty played game list for empty file", async () => {
-    const s = getNamesAndGamesCount("tao_maxim_file/emptyNameList.txt");
-    const expected = new Map<string, number>();
-    expect(s).toStrictEqual(expected);
-  });
-
-  it("should return amount of game won by player1 for single line file", async () => {
-    const s = getNamesAndWinCount("tao_maxim_file/singleLinePlayer1RoyalFlush.txt");
-    expect(s.get("Jane")).toBe(1);
-  });
-
-  it("should return amount of game won by player2 for single line file", async () => {
-    const s = getNamesAndWinCount("tao_maxim_file/singleLinePlayer2RoyalFlush.txt");
-    expect(s.get("Mike")).toBe(1);
-  });  
-
-});
+//   it("should return names list for multiple line file", async () => {
+//     const s = getNames("tao_maxim_file/multipleLine.txt");
+//     const expected = ["Jane", "Mike", "Wu", "Ken"];
+//     expect(s).toStrictEqual(new Set(expected));
+//   });
+// });
 
 
 describeWithDB("WinRateCollector ", () => {
