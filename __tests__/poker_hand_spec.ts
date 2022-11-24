ab30aab9453b431830cf220857f5910dc0e927d6
Newiihuhu <Ninewii_huhu@hotmail.com>
2022-11-24 12:08:10 +0700

    tcring

--- __tests__/poker_hand_spec.ts
+++ __tests__/poker_hand_spec.ts
@@ -1,53 +1,53 @@
 import { isPair } from "../src/services/PockerHandRanker";
 import {
-  winRateFromFile,
-  loadData,
-  getTotalGames,
+    winRateFromFile,
+    loadData,
+    getGameRecords,
 } from "../src/services/PokerHandService";
 import describeWithDB from "../test_helpers/describeWithDB";
 
 describe("Cheater Dectector", () => {
-  it("when load empty file should return empty list", () => {
-    const file = "";
+    it("when load empty file should return empty list", () => {
+        const file = "";
 
-    const result = winRateFromFile(file);
+        const result = winRateFromFile(file);
 
-    expect(result).toBe("");
-  });
+        expect(result).toBe("");
+    });
 });
 
 describe("PokerHandRanker", () => {
-  it("should return true when hand is flush", () => {
-    expect(true).toBe(true);
-  });
-
-  describe("Validate Hight Card", () => {
-    it("should be get player win with hight card", () => {
-      expect(true).toBe(true);
+    it("should return true when hand is flush", () => {
+        expect(true).toBe(true);
     });
-  });
+
+    describe("Validate Hight Card", () => {
+        it("should be get player win with hight card", () => {
+            expect(true).toBe(true);
+        });
+    });
 });
 
 describe("Games Counting", () => {
-  it("should show total games as 0 when no game data", () => {
-    expect(getTotalGames()).toBe(0);
-  });
-});
+    it("should show 0 when player have no game record", () => {
+        expect(getGameRecords("Mike")).toBe(0);
+    });
+})
 
 describeWithDB("Game Data Loader", () => {
-  it("should return 0 when there is no record", () => {
-    expect(loadData("")).toBe("ok");
-  });
+    it("should return 0 when there is no record", () => {
+        expect(loadData("")).toBe("ok");
+    });
 });
 it("shouldn't save any data when there is no record", () => {
-  expect(loadData("")).toBe("ok");
+    expect(loadData("")).toBe("ok");
 });
 
 it("shouldn't save any data when there is no record", () => {
-  expect(loadData("")).toBe("ok");
+    expect(loadData("")).toBe("ok");
 });
 describe("Hand", () => {
-  it("should return true for pair", () => {
-    expect(isPair("")).toBe(true);
-  });
+    it("should return true for pair", () => {
+        expect(isPair("")).toBe(true);
+    });
 });
--- src/services/PokerHandService.ts
+++ src/services/PokerHandService.ts
@@ -1,24 +1,24 @@
 import PlayerModel from "../models/player";
 
 export const winnerOfHighCard = (game: string) => {
-  return "Mike";
+    return "Mike";
 };
 
 const create = async (player) => {
-  return await PlayerModel.create(player);
+    return await PlayerModel.create(player);
 };
 
 export function winRateFromFile(file: string): string {
-  return "";
+    return "";
 }
 
 export function loadData(fileName: string) {
-  return "ok";
+    return "ok";
 }
 
-export const getTotalGames = (): number => {
-  return 0;
-};
+export const getGameRecords = (playerName: string): number => {
+    return 0
+}
 
 
 export default { create };
