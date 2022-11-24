35b999a37723e2a808da04c4269dd9248b60f4ae
builder <builder@odds.team>
2022-11-24 11:28:42 +0700

    tcring

--- __tests__/poker_hand_spec.ts
+++ __tests__/poker_hand_spec.ts
@@ -1,11 +1,20 @@
 describe("Cheater Dectector", () => {
-    it("when load empty file should return empty list", () => {
+  it("when load empty file should return empty list", () => {});
 
+  describe("Games Counting", () => {
+    it("should show how many each player play the game", () => {
+      expect(true).toBe(true);
     });
+  });
+});
 
-    describe("Games Counting", () => {
-        it("should show how many each player play the game", () => {
-            expect(true).toBe(true);
-        });
-    })
+describe("Games Counting", () => {
+  it("should show how many each player play the game", () => {
+    expect(true).toBe(true);
+  });
+});
+describe("Validate Hight Card", () => {
+  it("should be get player win with hight card", () => {
+    expect(true).toBe(true);
+  });
 });
