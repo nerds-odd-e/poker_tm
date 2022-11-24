9410711e95e025811dd79071f3e4ffc1f307a9a8
Jirat Chokdeeanan <jirat.cho@gmail.com>
2022-11-24 12:10:44 +0700

    tcring

--- __tests__/poker_hand_spec.ts
+++ __tests__/poker_hand_spec.ts
@@ -1,53 +1,51 @@
 import { isPair } from "../src/services/PockerHandRanker";
 import {
-    winRateFromFile,
-    loadData,
-    getGameRecords,
+  winRateFromFile,
+  loadData,
+  getGameRecords,
 } from "../src/services/PokerHandService";
 import describeWithDB from "../test_helpers/describeWithDB";
 
 describe("Cheater Dectector", () => {
-    it("when load empty file should return empty list", () => {
-        const file = "";
+  it("when load empty file should return empty list", () => {
+    const file = "";
 
-        const result = winRateFromFile(file);
+    const result = winRateFromFile(file);
 
-        expect(result).toBe("");
-    });
+    expect(result).toBe("");
+  });
 });
 
 describe("PokerHandRanker", () => {
-    it("should return true when hand is flush", () => {
-        expect(true).toBe(true);
-    });
+  it("should return true when hand is flush", () => {
+    expect(true).toBe(true);
+  });
 
-    describe("Validate Hight Card", () => {
-        it("should be get player win with hight card", () => {
-            expect(true).toBe(true);
-        });
+  describe("Validate Hight Card", () => {
+    it("should be get player win with hight card", () => {
+      expect(true).toBe(true);
     });
+  });
 });
 
 describe("Games Counting", () => {
-    it("should show 0 when player have no game record", () => {
-        expect(getGameRecords("Mike")).toBe(0);
-    });
+  it("should show 0 when player have no game record", () => {
+    expect(getGameRecords("Mike")).toBe(0);
+  });
 })
 
-describeWithDB("Game Data Loader", () => {
-    it("should return 0 when there is no record", () => {
-        expect(loadData("")).toBe("ok");
-    });
-});
-it("shouldn't save any data when there is no record", () => {
-    expect(loadData("")).toBe("ok");
+describe("Hand", () => {
+  it("should return true for pair", () => {
+    expect(isPair("")).toBe(true);
+  });
 });
 
-it("shouldn't save any data when there is no record", () => {
-    expect(loadData("")).toBe("ok");
-});
-describe("Hand", () => {
-    it("should return true for pair", () => {
-        expect(isPair("")).toBe(true);
-    });
-});
+describeWithDB("Game Data Loader", () => {
+  it("should return 0 when there is no record", () => {
+    expect(loadData("")).toBe(0)
+  })
+
+  it("should return 1 when there is 1 record", () => {
+    expect(loadData("one_game.txt")).toBe(1)
+  })
+})
