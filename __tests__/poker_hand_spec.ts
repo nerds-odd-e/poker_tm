30a349a98258422d5506e29c84d2d0e3d3c191c0
Newiihuhu <Ninewii_huhu@hotmail.com>
2022-11-24 11:51:19 +0700

    tcring

--- __tests__/poker_hand_spec.ts
+++ __tests__/poker_hand_spec.ts
@@ -16,12 +16,6 @@ describe("PokerHandRanker", () => {
         expect(true).toBe(true);
     });
 
-    describe("Games Counting", () => {
-        it("should show how many each player play the game", () => {
-            expect(true).toBe(true);
-        });
-    });
-
     describe("Validate Hight Card", () => {
         it("should be get player win with hight card", () => {
             expect(true).toBe(true);
