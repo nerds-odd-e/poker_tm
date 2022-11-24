87efc2997d0579d93c5bc7675ec1bdfb6c48c715
builder <builder@odds.team>
2022-11-24 13:26:31 +0700

    tcring

--- src/services/PokerHandService.ts
+++ src/services/PokerHandService.ts
@@ -1,30 +1,30 @@
 import * as fs from "fs";
 import * as path from "path";
-import PlayerModel from '../models/player';
+import PlayerModel from "../models/player";
 
-export const winnerOfHighCard = (game: string) => {
-    return "Mike";
+export const winnerOfHighCard = (gameRaw: string) => {
+  const game = gameRaw.split(" ");
+  return game[6];
 };
 
 const create = async (player) => {
-    return await PlayerModel.create(player);
+  return await PlayerModel.create(player);
 };
 
 export function winRateFromFile(file: string): string {
-    return "";
+  return "";
 }
 
 export function loadData(fileName: string) {
-    if (fileName === '') return 0
-    const filePath = path.join(__dirname, '../../example_data/one_game.txt');
-    const buffer = fs.readFileSync(filePath, "utf-8");
+  if (fileName === "") return 0;
+  const filePath = path.join(__dirname, "../../example_data/one_game.txt");
+  const buffer = fs.readFileSync(filePath, "utf-8");
 
-    return buffer.split("\n").length
+  return buffer.split("\n").length;
 }
 
 export const getGameRecords = (playerName: string): number => {
-    return 0
-}
-
+  return 0;
+};
 
 export default { create };
