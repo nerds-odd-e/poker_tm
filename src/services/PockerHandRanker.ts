838329f02216b1b035549bbd38561e62c0cd286a
Chalittaya Nimpattanawong <aux-chalittaya.n@tech.jago.com>
2022-11-24 14:14:35 +0700

    tcring

--- src/services/PockerHandRanker.ts
+++ src/services/PockerHandRanker.ts
@@ -1,7 +1,11 @@
 export function isPair(game: string): Boolean {
    const firstHand = game.split(" ").slice(1, 6)
    const secondHand = game.split(" ").slice(7)
-  return false;
+  return isHandPair(firstHand) || isHandPair(secondHand);
+}
+
+export function isHandPair(hand: string[]): boolean {
+    return false;
 }
 
 function isFlush(hands: Array<string>) {
