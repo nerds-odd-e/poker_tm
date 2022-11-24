ffba92f7a72b15f0a64cf07a00e4c1afe3994f1c
builder <builder@odds.team>
2022-11-24 11:48:54 +0700

    tcring

--- __tests__/poker_hand_spec.ts
+++ __tests__/poker_hand_spec.ts
@@ -50,3 +50,13 @@ describe("Cheater Dectector", () => {
     });
   });
 });
+
+describeWithDB("Game Data Loader", () => {
+  it("should return 0 when there is no record", () => {
+    expect(loadData("")).toBe(0);
+  });
+
+  it("should return 1 when there is 1 record", () => {
+    expect(loadData("one_game.txt")).toBe(1);
+  });
+});
